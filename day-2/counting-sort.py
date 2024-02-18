742ee2c3becf601b81f034d94bde8c0657f210b8
zareen nidhi <zareennidhi@zareens-MacBook-Air.local>
2024-02-19 02:04:23 +0600

    counting sort

---
+++ day-2/counting-sort.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Write your code here
+    index_list = [0] * 100
+    for item in arr:
+        index_list[item] += 1
+    return index_list
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
